0afff0efea6d3d3a1f44144a51c67f59590d838b
disastah! <highkal.wahyu7@gmail.com>
2022-04-22 05:30:56 +0700

    complete relation

--- app/src/main/java/com/dicoding/mystudentdata/database/StudentEntity.kt
+++ app/src/main/java/com/dicoding/mystudentdata/database/StudentEntity.kt
@@ -27,18 +27,45 @@ data class Course(
 data class StudentAndUniversity(
     @Embedded
     val student: Student,
+
     @Relation(
         parentColumn = "univId",
         entityColumn = "universityId"
     )
     val university: University? = null
 )
+
 data class UniversityAndStudent(
     @Embedded
     val university: University,
+
     @Relation(
         parentColumn = "universityId",
         entityColumn = "univId"
     )
     val student: List<Student>
+)
+
+@Entity(primaryKeys = ["sId", "cId"])
+data class CourseStudentCrossRef(
+    val sId: Int,
+    @ColumnInfo(index = true)
+    val cId: Int,
+)
+
+data class StudentWithCourse(
+    @Embedded
+    val studentAndUniversity: StudentAndUniversity,
+
+    @Relation(
+        parentColumn = "studentId",
+        entity = Course::class,
+        entityColumn = "courseId",
+        associateBy = Junction(
+            value = CourseStudentCrossRef::class,
+            parentColumn = "sId",
+            entityColumn = "cId"
+        )
+    )
+    val course: List<Course>
 )
